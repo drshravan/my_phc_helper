74895e46939330eb64c824873741f69dbd0067fa
drshravan <siddulashravan7@gmail.com>
2025-12-21 16:02:15 +0530

    s

---
+++ lib/screens/phc_info/phc_staff_list.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class PhcStaffList extends StatefulWidget {
+  const PhcStaffList({super.key});
+
+  @override
+  State<PhcStaffList> createState() => _PhcStaffListState();
+}
+
+class _PhcStaffListState extends State<PhcStaffList> {
+
+  List subcenters = [
+                  "Chilpur"
+                  "Chinnapendyal",
+                  "Malkapur",
+                  "Nashkal",
+                  "Pallagutta",
+                  "Rajavaram",
+                  "Sripathipally",
+                ];
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
